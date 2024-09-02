3cca124e71b7ee3b67242d893b6eaff84ae0e695
RoescenAbiePajaro <157484846+RoescenAbiePajaro@users.noreply.github.com>
2024-09-02 01:20:25 -0500

    Files terminal

---
+++ utils/server.js
@@ -0,0 +1,25 @@
+const express = require('express') const morgan = require('morgan')
+
+const bodyParser = require('body-parser')
+
+const cors require('cors')
+
+const dotanv = require('dotenv')
+
+require('colors')
+
+//rest object
+
+const app express()
+
+//middlwares
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use(morgan("dev"))
